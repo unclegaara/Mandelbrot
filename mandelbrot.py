98b9578d17babaa1e3e0e19439a0e357a0a2eb1b
Max Kuzmin <kazekage.kun@gmail.com>
2013-05-16 14:49:46 +0400

    add unzoom

--- mandelbrot.py
+++ mandelbrot.py
@@ -1,28 +1,40 @@
 import pygame, sys
 
+class State(object):
+    def __init__(self, xmin, xmax, ymin, ymax):
+        self.xmin = xmin
+        self.xmax = xmax
+        self.ymin = ymin
+        self.ymax = ymax
+
+    def zoom(self, x, y):
+        w = (self.xmax - self.xmin) / scale
+        h = (self.ymax - self.ymin) / scale
+        xmin = x - w / 2
+        ymax = y + h / 2
+        xmax = x + w / 2
+        ymin = y - h / 2
+        return State(xmin, xmax, ymin, ymax)
+
+    def getpos(self, i, j):
+        x = self.xmin + (float(i) * (self.xmax - self.xmin) / width)
+        y = self.ymax - (float(j) * (self.ymax - self.ymin) / height)
+        return x, y
+
+
 width = 640
 height = (width / 3) * 2
-xmin = -2.0
-xmax = 1.0
-ymin = -1.0
-ymax = 1.0
 linecolor = (0, 0, 0)
 background = (255, 255, 255)
 maxi = 255
 scale = 4.0
+steplist = [State(xmin=-2.0, xmax=1.0, ymin=-1.0, ymax=1.0)]
 
 
-def getpos(i, j):
-    x = xmin + (float(i) * (xmax - xmin) / width)
-    y = ymax - (float(j) * (ymax - ymin) / height)
-    return x, y
-
-
-def draw(screen):
-    print xmax, ymax, xmin, ymin
+def draw(screen, state):
     for i in xrange(0, width - 1):
         for j in xrange(0, height - 1):
-            x, y = getpos(i, j)
+            x, y = state.getpos(i, j)
             c = complex(x, y)
             z = complex(0.0, 0.0)
             k = 0
@@ -46,22 +58,23 @@ def main():
     pygame.display.set_mode((width, height))
     pygame.display.set_caption('Mandelbrot Set')
     screen = pygame.display.get_surface()
-    draw(screen)
+    draw(screen, steplist[-1])
     while True:
         events = [pygame.event.wait()]
         for event in events:
             if event.type == pygame.QUIT:
                 sys.exit()
             elif event.type == pygame.MOUSEBUTTONDOWN:
-                i, j = event.pos
-                x, y = getpos(i, j)
-                w = (xmax - xmin) / scale
-                h = (ymax - ymin) / scale
-                xmin = x - w / 2
-                ymax = y + h / 2
-                xmax = x + w / 2
-                ymin = y - h / 2
-                draw(screen)
+                if event.button == 1:
+                    i, j = event.pos
+                    x, y = steplist[-1].getpos(i, j)
+                    steplist.append(steplist[-1].zoom(x, y))
+                    draw(screen, steplist[-1])
+                elif event.button == 3:
+                    if len(steplist) > 1:
+                        steplist.pop()
+                        draw(screen, steplist[-1])
+
 
 if __name__=='__main__':
     main()
